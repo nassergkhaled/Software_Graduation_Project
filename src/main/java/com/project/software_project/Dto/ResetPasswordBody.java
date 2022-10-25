e6b71b5b66d6047a674cb04d0c36026cac844829
Nasser Al-den Ghassan Shareef Khaled <80484566+nasserkh12@users.noreply.github.com>
2022-10-25 08:32:22 +0300

    Delete src directory

--- src/main/java/com/project/software_project/Dto/ResetPasswordBody.java
+++
@@ -1,9 +0,0 @@
-package com.project.software_project.Dto;
-
-import lombok.Data;
-
-@Data
-public class ResetPasswordBody {
-    public Integer id;
-    public String newPassword;
-}
--- src/main/src/main/java/com/project/software_project/Dto/LogInBody.java
+++
@@ -1,9 +0,0 @@
-package com.project.software_project.Dto;
-
-import lombok.Data;
-
-@Data
-public class LogInBody {
-    public Integer id;
-    public String password;
-}
